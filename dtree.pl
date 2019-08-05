7e74b9ff4fb5b399c8f592ac9f717fadd8aead1f
vinxavier <42624994+vinxavier@users.noreply.github.com>
2019-08-05 17:38:11 -0300

    Update dtree.pl

--- dtree.pl
+++ dtree.pl
@@ -27,6 +27,11 @@ opcquaternaria(A, B, C, D) :-
 respperg(X, Y) :-
 	format("A resposta foi ~w. ~n ~n Digite o numero correspondente a opcao para ~w: ~n", [X,Y]).
 
+% Mostra a resposta e encerra programa
+respencerra(X,Y) :-
+	format("A resposta foi ~w. ~n ~n",[X]),
+	encerra(Y).
+
 % Encerra a execução do programa
 encerra(X) :-
 	nl, write("________________________________DIAGNOSTICO:________________________________"), nl,nl,nl,
@@ -49,9 +54,14 @@ diagnostico(Other) :-
 	write("################## IMPOSSIVEL DE DETERMINAR ##################").
 	
 
+	
+
+%%%%%%%%%%%%%%%%%%%%%%
+% Raiz da arvore
+%%%%%%%%%%%%%%%%%%%%%%
 % Verifica o código digitado na ASCII, valor de ASCII de 1 é 49, e de 2 é 50.
 % As chamadas de by pass iria seguir para um rumo diferente na arvore de acordo com a resposta da primeira pergunta, 
-% logo precisamos diferenciar com 1 e 2.	
+% logo precisamos diferenciar com 1 e 2.
 bllymphc(49) :-
 	respperg("NAO", "By Pass"),
 	opcbinaria,
@@ -65,6 +75,10 @@ bllymphc(50) :-
 bllymphc(Other) :-
 	digitedenovo,
 	get_single_char(C), bllymphc(C).
+
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 1 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
 	
 % bypass1, quando bllymphc é nao.
 bypass1(49) :-
@@ -95,10 +109,15 @@ bypass2(Other) :-
 	digitedenovo,
 	get_single_char(C), bypass2(C).
 
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 2 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
+
 % dislocation1 quando bypass1 é nao
 dislocation1(49) :-
-	% TODO : implementar
-	encerra(0).
+	respperg("NAO", "Block of Affere"),
+	opcbinaria,
+	get_single_char(C), blockofaffere1(C).
 
 dislocation1(50) :-
 	respperg("SIM", "Exclusion Of Nodes"),
@@ -113,10 +132,10 @@ dislocation1(Other) :-
 dislocation2(49) :-
 	respperg("NAO", "Block of Affere"),
 	opcbinaria,
-	get_single_char(C), blockofaffere1(C).
+	get_single_char(C), blockofaffere2(C).
 
 dislocation2(50) :-
-	write("A resposta foi SIM"),nl,nl,
+	respperg("NAO", "Lymphatics"),
 	opcquaternaria("Normal","Arched","Deformed","Displaced"),
 	get_single_char(C), lymphatics1(C).
 
@@ -126,7 +145,7 @@ dislocation2(Other) :-
 
 % earlyuptake1 quando bypass2 é sim
 earlyuptake1(49) :-
-	respperg("NAO", " Bl. of Lymph. S"),
+	respperg("NAO", "Bl. of Lymph. S"),
 	opcbinaria,
 	get_single_char(C), bllymphs(C).
 
@@ -139,6 +158,49 @@ earlyuptake1(Other) :-
 	digitedenovo,
 	get_single_char(C), earlyuptake1(C).
 
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 3 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
+
+% blockofaffere1 quando dislocation1 é não
+blockofaffere1(49) :-
+	respperg("NAO", "Exclusion Of Nodes"),
+	opcbinaria,
+	get_single_char(C), exclusion2(C).
+
+blockofaffere1(50) :-
+	respperg("SIM", "Early Uptake"),
+	opcbinaria,
+	get_single_char(C), earlyuptake2(C).
+
+blockofaffere1(Other) :-
+	digitedenovo,
+	get_single_char(C), blockofaffere1(C).
+
+% blockofaffere2 quando dislocation2 é Não
+blockofaffere2(49) :-
+	write("A resposta foi NAO"),nl,nl,
+	encerra(4).
+
+blockofaffere2(50) :-
+	respencerra("SIM", 2).
+
+blockofaffere2(Other) :-
+	digitedenovo,
+	get_single_char(C), blockofaffere2(C).
+
+% bllymphs caso earlyuptake1 seja nao
+bllymphs(49) :-
+	write("A resposta foi NAO"),nl,nl,
+	encerra(2).
+
+bllymphs(50) :-
+	respencerra("SIM", 4).
+
+bllymphs(Other) :-
+	digitedenovo,
+	get_single_char(C), bllymphs(C).
+
 % exclusion1 quando dislocation1 é sim
 exclusion1(49) :-
 	respperg("NAO", "Lymphatics"),
@@ -148,127 +210,239 @@ exclusion1(49) :-
 exclusion1(50) :-
 	respperg("SIM", "Early Uptake of"),
 	opcbinaria,
-	get_single_char(C), earlyuptake2(C).
+	get_single_char(C), earlyuptake3(C).
 
 exclusion1(Other) :-
 	digitedenovo,
 	get_single_char(C), exclusion1(C).
 
-% blockofaffere1 quando dislocation2 é Não
-blockofaffere1(49) :-
-	write("A resposta foi NAO"),nl,nl,
-	encerra(4).
+% lymphatics1 caso dislocation2 seja sim
+lymphatics1(49):-
+	respencerra("Normal", 0).
 
-blockofaffere1(50) :-
-	write("A resposta foi SIM"),nl,nl,
-	encerra(2).
+lymphatics1(50):-
+	respencerra("Arched", 0).
 
-blockofaffere1(Other) :-
+lymphatics1(51):-
+	respencerra("Deformed", 3).
+
+lymphatics1(52):-
+	respencerra("Displaced", 2).
+
+lymphatics1(Other):-
 	digitedenovo,
-	get_single_char(C), blockofaffere1(C).
-
-% bllymphs caso earlyuptake1 seja nao
-bllymphs(49) :-
-	write("A resposta foi NAO"),nl,nl,
-	encerra(2).
-
-bllymphs(50) :-
-	write("A resposta foi SIM"),nl,nl,
-	encerra(4).
-
-bllymphs(Other) :-
-	digitedenovo,
-	get_single_char(C), bllymphs(C).
+	get_single_char(C), lymphatics1(C).
 
 % regeneration caso earlyuptake1 seja sim
 regeneration(49) :-
-	write("A resposta foi NAO"),nl,nl,
-	encerra(2).
+	respencerra("NAO", 2).
 
 regeneration(50) :-
-	write("A resposta foi SIM"),nl,nl,
-	encerra(3).
+	respencerra("SIM", 3).
 
 regeneration(Other) :-
 	digitedenovo,
 	get_single_char(C), regeneration(C).
 
-% lymphatics1 caso dislocation2 seja sim
-lymphatics1(49):
-	write("A resposta foi Normal"),nl,nl,
-	encerra(0).
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 4 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
 
-lymphatics1(50):
-	write("A resposta foi Arched"),nl,nl,
-	encerra(0).
+% exclusion2 caso blackofaffere1 seja não
+exclusion2(49) :-
+	respencerra("NAO", 1).
 
-lymphatics1(51):
-	write("A resposta foi Deformed"),nl,nl,
-	encerra(3).
+exclusion2(50) :-
+	respperg("SIM", "Extravasates"),
+	opcbinaria,
+	get_single_char(C), extravasates1(C).
 
-lymphatics1(52):
-	write("A resposta foi Displaced"),nl,nl,
-	encerra(2).
-
-lymphatics1(Other):
+exclusion2(Other) :-
 	digitedenovo,
-	get_single_char(C), lymphatics1(C).
+	get_single_char(C), exclusion2(C).
+
+% earlyuptake2 quando blockofaffere1 é sim
+earlyuptake2(49):-
+	respencerra("NAO", 2).
+
+earlyuptake2(50):-
+	respperg("SIM", "Exclusion of"),
+	opcbinaria,
+	get_single_char(C), exclusion3(C).
+
+earlyuptake2(Other):-
+	digitedenovo,
+	get_single_char(C), earlyuptake2(C).
 
 % lymphatics2 caso exclusion1 seja não
-lymphatics2(49):
-	write("A resposta foi Normal"),nl,nl,
-	encerra(0).
+lymphatics2(49):-
+	respencerra("Normal", 0).
 
-lymphatics2(50):
-	write("A resposta foi Arched"),nl,nl,
-	encerra(2).
+lymphatics2(50):-
+	respencerra("Arched", 2).
 
-lymphatics2(51):
-	write("A resposta foi Deformed"),nl,nl,
-	encerra(0).
+lymphatics2(51):-
+	respencerra("Deformed", 0).
 
-lymphatics2(52):
-	write("A resposta foi Displaced"),nl,nl,
-	encerra(3).
+lymphatics2(52):-
+	respencerra("Displaced", 3).
 
-lymphatics2(Other):
+lymphatics2(Other):-
 	digitedenovo,
 	get_single_char(C), lymphatics2(C).
 
-% earlyuptake2 quando exclusion1 é sim
-earlyuptake2(49):
+% earlyuptake3 quando exclusion1 é sim
+earlyuptake3(49):-
 	respperg("NAO", "Lymphatics"),
 	opcquaternaria("Normal","Arched","Deformed","Displaced"),
 	get_single_char(C), lymphatics3(C).
 
-earlyuptake2(50):
+earlyuptake3(50):-
 	respperg("SIM", "Extravasates"),
 	opcbinaria,
-	get_single_char(C), extravasates(C).
+	get_single_char(C), extravasates2(C).
 
-earlyuptake2(Other):
+earlyuptake3(Other):-
 	digitedenovo,
-	get_single_char(C), earlyuptake2(C).
+	get_single_char(C), earlyuptake3(C).
 
-% lymphatics2 quando earlyuptake2 for nao
-lymphatics3(49):
-	write("A resposta foi Normal"),nl,nl,
-	encerra(0).
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 5 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
 
-lymphatics3(50):
-	write("A resposta foi Arched"),nl,nl,
-	encerra(2).
+% exclusion3 caso earlyuptake2 seja sim
+exclusion3(49) :-
+	respencerra("NAO", 2).
 
-lymphatics3(51):
-	write("A resposta foi Deformed"),nl,nl,
-	encerra(3).
+exclusion3(50) :-
+	respperg("SIM", "Extravasates"),
+	opcbinaria,
+	get_single_char(C), extravasates3(C).
 
-lymphatics3(52):
-	write("A resposta foi Displaced"),nl,nl,
-	encerra(0).
+exclusion3(Other) :-
+	digitedenovo,
+	get_single_char(C), exclusion3(C).
 
-lymphatics3(Other):
+% extravasets1 caso exclusion2 seja sim
+extravasates1(49) :-
+	respencerra("NAO", 2).
+
+extravasates1(50) :-
+	respencerra("SIM", 3).
+
+extravasates1 :-
+	digitedenovo,
+	get_single_char(C), extravasates1(C).
+
+% extravasets2 caso earlyuptake3 seja sim
+extravasates2(49) :-
+	respperg("NAO", "Special Forms"),
+	writef("\t 1 - No"), nl,
+	writef("\t 2 - Chalices"), nl,
+	writef("\t 3 - Vesicles"), nl,
+	get_single_char(C), specialforms(C).
+
+extravasates2(50) :-
+	respperg("NAO", "Block of Affere"),
+	opcbinaria,
+	get_single_char(C), blockofaffere3(C).
+
+extravasates2 :-
+	digitedenovo,
+	get_single_char(C), extravasates2(C).
+
+% lymphatics3 quando earlyuptake3 for nao
+lymphatics3(49):-
+	respencerra("Normal", 0).
+
+lymphatics3(50):-
+	respencerra("Arched", 2).
+
+lymphatics3(51):-
+	respencerra("Deformed", 3).
+
+lymphatics3(52):-
+	respencerra("Displaced", 0).
+
+lymphatics3(Other):-
 	digitedenovo,
 	get_single_char(C), lymphatics3(C).
 
-% extravasates caso earlyuptake2 for sim
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 6 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
+% extravasates3(é igual o extravasates1) caso exclusion3 seja sim
+extravasates3(49):-
+	extravasates1(49).
+
+extravasates3(50):-
+	extravasates1(50).
+
+extravasates3(Other):-
+	extravasates1(Other).
+
+% specialforms caso extravasets2 seja nao
+specialforms(49):-
+	respperg("NO", "Number of nodes"),
+	writef("\t 1 - (0 - 9)"), nl,
+	writef("\t 2 - (10 - 19)"), nl,
+	writef("\t 3 - (>=20)"), nl,
+	get_single_char(C), nofnodes(C).
+
+specialforms(50):-
+	respencerra("Chalices", 2).
+
+specialforms(51):-
+	respencerra("Vasicles", 0).
+
+specialforms(Other):-
+	digitedenovo,
+	get_single_char(C), specialforms(C).
+
+% blockofaffere3 caso extravasets2 seja sim
+blockofaffere3(49):-
+	respencerra("NAO", 3).
+
+blockofaffere3(50):-
+	respperg("NAO", "Changes in Structure"),
+	writef("\t 1 - No, Grainy, Drop-like, Coarse, Reticular or Stripped"), nl,
+	writef("\t 2 - Diluted"), nl,
+	writef("\t 3 - Faint"), nl,
+	get_single_char(C), changesinstructure(C).
+
+blockofaffere3(Other):-
+	digitedenovo,
+	get_single_char(C), blockofaffere3(C).
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+% Nível 7 da arvore
+%%%%%%%%%%%%%%%%%%%%%%
+
+% nofnodes caso specialforms seja No
+nofnodes(49):-
+	respencerra("(0-9)",2).
+
+nofnodes(50):-
+	respencerra("(10-19)",3).
+
+nofnodes(51):-
+	respencerra("(>=20)",0).
+
+nofnodes(Other):-
+	digitedenovo,
+	get_single_char(C), nofnodes(C).
+
+% changesinstructure quando blockofaffere3 é não
+changesinstructure(49):-
+	respencerra("No, Grainy, Drop-like, Coarse, Reticular or Stripped",0).
+
+changesinstructure(50):-
+	respencerra("Diluted",3).
+
+changesinstructure(51):-
+	respencerra("Faint",2).
+
+changesinstructure(Other):-
+	digitedenovo,
+	get_single_char(C), changesinstructure(C).
